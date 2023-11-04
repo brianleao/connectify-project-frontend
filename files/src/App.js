d528e3e866b04bb5e51929ea06a85d4818ad95ed
Brian Leao <brianleao@icloud.com>
2023-11-04 18:39:46 -0300

    restored navgation and footer

--- files/src/App.js
+++ files/src/App.js
@@ -5,15 +5,19 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './components/Home';
 import Contato from './components/Contato';
 import Empresa from './components/Empresa';
+import Navigation from './layout/Navigation';
+import Footer from './layout/Footer';
 
 function App() {
   return (
     <Router>
+      <Navigation/>
             <Routes>
                 <Route exact path="/" element={<Home />} />
                 <Route path="/contato" element={<Contato />} />
                 <Route path="/empresa" element={<Empresa />} />
             </Routes>
+      <Footer/>
     </Router>
     
   );
