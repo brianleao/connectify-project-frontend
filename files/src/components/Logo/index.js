7e47f3599c17477f67cbf8ebfb3c6626306e0479
Brian Leao <brianleao@icloud.com>
2023-11-20 09:56:57 -0300

    ajust in home logo

--- files/src/components/Logo/index.js
+++ files/src/components/Logo/index.js
@@ -1,10 +1,11 @@
 import {Link} from 'react-router-dom'
 import LogoConnectify from '../../layout/images/logo_connectify.png'
+import styles from './index.css'
 
 function Logo () {
     return (
        <Link to="/">
-            <img src={LogoConnectify}/>
+            <img src={LogoConnectify} className={styles.logo_connectify}/>
        </Link>
     )
 }
