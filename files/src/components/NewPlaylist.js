708427dc6eb44d7bb750b67afc2b601d612b720a
Brian Leao <brianleao@icloud.com>
2023-11-27 13:59:54 -0300

    create itens html in NewPlaylist page

--- files/src/components/NewPlaylist.js
+++ files/src/components/NewPlaylist.js
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom"
 import styles from "./NewPlaylist.module.css"
+import React, {useState} from 'react'
 
 export default function NewPlaylist () {
+    
+    const [createPlaylist, setCreatePlaylist] = useState();
+    
     return (
         <div>
-
+            <h1>De um nome à sua Playlist</h1>
+            <input type="text" onChange={(e) => setCreatePlaylist(e.target.value)}/>
+            <button>Criar</button>
         </div>
     )
 }
